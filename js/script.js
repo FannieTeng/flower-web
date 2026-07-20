c17b6b9e24021881e903c0754fba231b7f4d9d9f
FannieTeng <fannieteng@users.noreply.github.com>
2026-07-20 09:42:16 +0800

    Track add to cart events

--- js/script.js
+++ js/script.js
@@ -9,4 +9,14 @@ document.addEventListener("DOMContentLoaded", () => {
       touch: true
     });
   }
+
+  const cartButtons = document.querySelectorAll(".btn-cart");
+
+  cartButtons.forEach((button) => {
+    button.addEventListener("click", (event) => {
+      event.preventDefault();
+      trackAddToCart();
+      alert("您已將商品加入購物車");
+    });
+  });
 });
